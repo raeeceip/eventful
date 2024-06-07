12d369edbf558e8d5f3c924d3fe5718236767527
Chisom Chibogu <chisom.chibogu@fullscript.com>
2024-06-07 17:22:39 -0400

    will add more tests later

---
+++ db/db_test.go
@@ -0,0 +1,23 @@
+// FILEPATH: /Users/chisom.chibogu/dev/eventful/db/db_test.go
+
+package db
+
+import (
+	"database/sql"
+	"testing"
+)
+
+func TestConnectToMySQLDatabase(t *testing.T) {
+	// Set up the test environment
+	// ...
+
+	// Connect to the MySQL database
+	db, err := sql.Open("sql", "username:password@tcp(localhost:3306)/database_name")
+	if err != nil {
+		t.Fatalf("Failed to connect to SQL database: %v", err)
+	}
+	defer db.Close()
+
+	// Perform assertions or further tests
+	// ...
+}
